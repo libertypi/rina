d59d02ceeb32469b11ff3efff2ccfb2d20acc563
libertypi <libertypi@gmail.com>
2021-01-25 19:48:52 +0800

    .

--- tools/make_mgs.py
+++ tools/make_mgs.py
@@ -19,39 +19,17 @@ from lxml.etree import XPath
 from lxml.html import fromstring
 from urllib3 import Retry
 
-FILE = Path(__file__).resolve()
-sys.path.insert(0, str(FILE.parent.parent))
+path = Path(__file__).resolve()
+JSON_FILE = path.with_name("mgs.json")
+path = path.parent.parent
+PY_FILE = path.joinpath("avinfo", "_mgs.py")
+sys.path.insert(0, str(path))
+del path
+
 from avinfo._mgs import mgs_map
 
-
-def _init_session():
-    session = requests.Session()
-    session.cookies.set_cookie(
-        requests.cookies.create_cookie(domain="mgstage.com",
-                                       name="adc",
-                                       value="1"))
-    session.headers.update({
-        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/88.0.4324.104 Safari/537.36'
-    })
-    adapter = requests.adapters.HTTPAdapter(
-        max_retries=Retry(total=5, read=3, backoff_factor=0.1))
-    session.mount("http://", adapter)
-    session.mount("https://", adapter)
-    return session
-
-
-def get_tree(url):
-    try:
-        response = session.get(url, timeout=(6.1, 60))
-        response.raise_for_status()
-    except requests.HTTPError:
-        pass
-    except requests.RequestException as e:
-        print(e, file=sys.stderr)
-    else:
-        return fromstring(response.content, base_url=response.url)
+DOMAIN = "https://www.mgstage.com"
+ENTRY_PAGE = "/ppv/makers.php?id=osusume"
 
 
 def parse_args():
@@ -73,25 +51,54 @@ def parse_args():
     return parser.parse_args()
 
 
-def get_mgs_result(local: bool) -> list:
+def _init_session():
+    session = requests.Session()
+    session.cookies.set_cookie(
+        requests.cookies.create_cookie(domain="mgstage.com",
+                                       name="adc",
+                                       value="1"))
+    session.headers.update({
+        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/88.0.4324.104 Safari/537.36'
+    })
+    adapter = requests.adapters.HTTPAdapter(
+        max_retries=Retry(total=5, read=3, backoff_factor=0.1))
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
+    return session
+
+
+def get_tree(url: str):
+    try:
+        response = session.get(url, timeout=(6.1, 60))
+        response.raise_for_status()
+    except requests.HTTPError:
+        pass
+    except requests.RequestException as e:
+        print(e, file=sys.stderr)
+    else:
+        return fromstring(response.content, base_url=response.url)
+
+
+def get_mgs_result(local: bool = False) -> list:
     """Get MGS scrape result from web or json.
     
     Returns: a list of 3-items dict
     """
 
-    json_file = FILE.with_name("mgs_scan.json")
     if local:
-        with open(json_file, "r", encoding="utf-8") as f:
+        with open(JSON_FILE, "r", encoding="utf-8") as f:
             return json.load(f)
 
     result = defaultdict(set)
     matcher = re.compile(r'/([0-9]*)([A-Za-z]{2,10})-([0-9]{2,8})/?$').search
     xp_id = XPath(
         '//article[@id="center_column"]'
-        '//div[@class="rank_list"]//h5/a/@href',
+        '//div[@class="rank_list"]//li/h5/a/@href',
         smart_strings=False)
 
-    for tree in _scan_mgs("/ppv/makers.php?id=osusume"):
+    for tree in _scan_mgs():
         try:
             print(f"Processing: {tree.base_url}")
         except AttributeError:
@@ -106,16 +113,15 @@ def get_mgs_result(local: bool) -> list:
     } for k, v in result.items()]
 
     result.sort(key=itemgetter("freq"), reverse=True)
-    with open(json_file, "w", encoding="utf-8") as f:
+    with open(JSON_FILE, "w", encoding="utf-8") as f:
         json.dump(result, f, indent=4)
 
     return result
 
 
-def _scan_mgs(url):
-    """input in initial url, output trees of maker pages."""
+def _scan_mgs(page: str = ENTRY_PAGE):
+    """input an entry page, output trees of maker pages."""
 
-    domain = "https://www.mgstage.com/"
     xp_maker = XPath(
         '//div[@id="maker_list"]/div[@class="maker_list_box"]'
         '/dl/dt/a[2]/@href[contains(., "search.php")]',
@@ -125,12 +131,12 @@ def _scan_mgs(url):
         '//a[contains(., "最後")]/@href)',
         smart_strings=False)
 
-    url = urljoin(domain, url)
-    tree = get_tree(url)
+    page = urljoin(DOMAIN, page)
+    tree = get_tree(page)
     makers = tree.xpath('//div[@id="maker_list"]/dl[@class="navi"]'
                         '/dd/a/@href[contains(., "makers.php")]')
-    makers = {urljoin(domain, u) for u in makers}
-    makers.discard(url)
+    makers = {urljoin(DOMAIN, u) for u in makers}
+    makers.discard(page)
 
     with ThreadPoolExecutor() as ex:
 
@@ -144,7 +150,7 @@ def _scan_mgs(url):
             makers.clear()
             makers.update(xp_maker(tree))
             pool.extend(
-                ex.submit(get_tree, urljoin(domain, u))
+                ex.submit(get_tree, urljoin(DOMAIN, u))
                 for u in makers
                 if u not in visited)
             visited.update(makers)
@@ -162,9 +168,9 @@ def _scan_mgs(url):
 
             if last.isdigit():
                 last = int(last) + 1
-                url = tree.base_url
+                page = tree.base_url
                 pool.extend(
-                    ex.submit(get_tree, f"{url}&page={i}")
+                    ex.submit(get_tree, f"{page}&page={i}")
                     for i in range(2, last))
 
         for tree in as_completed(pool):
@@ -193,7 +199,7 @@ def main():
     print(result)
     print(f"\nUnique IDs: {uniq_ids}",
           f"Unique prefixes: {uniq_prefix}",
-          f"Prefix digit range: {{{min(digit_len) or ''},{max(digit_len)}}}",
+          f'Prefix digit range: {{{min(digit_len) or ""},{max(digit_len)}}}',
           f"Dict size (old): {len(mgs_map)}",
           f"Dict size (new): {len(result)}",
           sep="\n")
@@ -204,9 +210,7 @@ def main():
 
     print("Writing changes to file...")
     indent = " " * 4
-    with open(FILE.parent.parent.joinpath("avinfo/_mgs.py"),
-              mode="w",
-              encoding="utf-8") as f:
+    with open(PY_FILE, "w", encoding="utf-8") as f:
         f.write("mgs_map = {\n")
         f.writelines(f'{indent}"{k}": "{v}",\n' for k, v in result.items())
         f.write("}\n")
