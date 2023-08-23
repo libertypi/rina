c93d96d67d62ea1a3a250d4889c1e890ff51b97a
libertypi <libertypi@gmail.com>
2023-08-23 03:59:08 -0400

    .

--- test/test.py
+++ test/test.py
@@ -5,7 +5,6 @@ from avinfo._utils import get_tree
 
 
 class Test_Scraper(unittest.TestCase):
-
     def _run_test(self, values: dict, source: str):
         for k, v in values.items():
             result = scraper.scrape(k)
@@ -16,163 +15,172 @@ class Test_Scraper(unittest.TestCase):
             self.assertEqual(source, result.source)
 
     def test_javbus(self):
-        source = 'javbus.com'
+        source = "javbus.com"
         values = {
-            "CZ016 vol.3": ('CZ016-3', '出合い頭4秒', 1504569600),
-            'SMbd-110 s 2 model 3': ('SMBD-110', '小西まりえ', 1412985600),
-            'bouga012 [cd2]': ('bouga012-2', '忘我素人', 1496275200),
-            'n0253': ('n0253', '無料校内中出', 1188518400),
-            '150605-KURUMI_KUMI': ('150605-KURUMI_KUMI', '美麗', 1433462400),
-            '150626 KURUMI': ('150626-KURUMI', '美少女', 1435276800)
+            "CZ016 vol.3": ("CZ016-3", "出合い頭4秒", 1504569600),
+            "SMbd-110 s 2 model 3": ("SMBD-110", "小西まりえ", 1412985600),
+            "bouga012 [cd2]": ("bouga012-2", "忘我素人", 1496275200),
+            "n0253": ("n0253", "無料校内中出", 1188518400),
+            "150605-KURUMI_KUMI": ("150605-KURUMI_KUMI", "美麗", 1433462400),
+            "150626 KURUMI": ("150626-KURUMI", "美少女", 1435276800),
         }
         self._run_test(values, source)
 
     def test_javdb(self):
-        source = 'javdb.com'
+        source = "javdb.com"
         values = {
-            'FC2PPV 1201745': ('FC2-1201745', '女の子', 1573776000),
-            'XXXAV 20879': ('XXX-AV-20879', '朝倉ことみ', 1565654400),
+            "FC2PPV 1201745": ("FC2-1201745", "女の子", 1573776000),
+            "XXXAV 20879": ("XXX-AV-20879", "朝倉ことみ", 1565654400),
         }
         self._run_test(values, source)
 
     def test_carib(self):
-        source = 'caribbeancom.com'
+        source = "caribbeancom.com"
         values = {
-            '[CARIB] 082920_001   (high) 3 haha 5':
-            ('082920-001-carib-high-3', '未来のきもち', 1598659200),
-            '120313_001 人 3': ('120313-001-carib', '麻倉憂', 1385683200),
+            "[CARIB] 082920_001   (high) 3 haha 5": (
+                "082920-001-carib-high-3",
+                "未来のきもち",
+                1598659200,
+            ),
+            "120313_001 人 3": ("120313-001-carib", "麻倉憂", 1385683200),
         }
         self._run_test(values, source)
 
     def test_caribpr(self):
-        source = 'caribbeancompr.com'
+        source = "caribbeancompr.com"
         values = {
-            '[HD]022114_777-caribpr-mid haha 5':
-            ('022114_777-caribpr-mid', 'レッドホット', 1392940800),
-            '090613_656-caribpr-whole1-hd':
-            ('090613_656-caribpr-whole1-hd', '全裸家政婦', 1378425600)
+            "[HD]022114_777-caribpr-mid haha 5": (
+                "022114_777-caribpr-mid",
+                "レッドホット",
+                1392940800,
+            ),
+            "090613_656-caribpr-whole1-hd": (
+                "090613_656-caribpr-whole1-hd",
+                "全裸家政婦",
+                1378425600,
+            ),
         }
         self._run_test(values, source)
 
     def test_1pon(self):
-        source = '1pondo.tv'
+        source = "1pondo.tv"
         values = {
-            '010617-460 1pon [1080p]':
-            ('010617_460-1pon-1080p', '鈴木さとみ', 1483660800)
+            "010617-460 1pon [1080p]": ("010617_460-1pon-1080p", "鈴木さとみ", 1483660800)
         }
         self._run_test(values, source)
 
     def test_10mu(self):
-        source = '10musume.com'
+        source = "10musume.com"
         values = {
-            '083014_01-10mu-whole1-psp':
-            ('083014_01-10mu-whole1-psp', '主人様', 1409356800)
+            "083014_01-10mu-whole1-psp": (
+                "083014_01-10mu-whole1-psp",
+                "主人様",
+                1409356800,
+            )
         }
         self._run_test(values, source)
 
     def test_paco(self):
-        source = 'pacopacomama.com'
+        source = "pacopacomama.com"
         values = {
-            '(pacopacomama) 071219-130': ('071219_130-paco', '鈴木', 1562889600),
-            '120618_394': ('120618_394-paco', '尾上若葉', 1544054400),
-            '030417_040-paco': ('030417_040-paco', '熟女', 1488585600)
+            "(pacopacomama) 071219-130": ("071219_130-paco", "鈴木", 1562889600),
+            "120618_394": ("120618_394-paco", "尾上若葉", 1544054400),
+            "030417_040-paco": ("030417_040-paco", "熟女", 1488585600),
         }
         self._run_test(values, source)
 
     def test_mura(self):
-        source = 'muramura.tv'
-        values = {'010216_333-mura': ('010216_333-mura', '美巨乳女優', 1451692800)}
+        source = "muramura.tv"
+        values = {"010216_333-mura": ("010216_333-mura", "美巨乳女優", 1451692800)}
         self._run_test(values, source)
 
     def test_heyzo(self):
-        source = 'heyzo.com'
+        source = "heyzo.com"
         values = {
-            '(heyzo) 1888': ('HEYZO-1888', '芸能人', 1545436800),
-            'heyzo-0755-c': ('HEYZO-0755-C', '彼氏目線', 1419465600),
-            'HEYZO-0947': ('HEYZO-0947', '美人姉妹', 1441756800),
+            "(heyzo) 1888": ("HEYZO-1888", "芸能人", 1545436800),
+            "heyzo-0755-c": ("HEYZO-0755-C", "彼氏目線", 1419465600),
+            "HEYZO-0947": ("HEYZO-0947", "美人姉妹", 1441756800),
         }
         self._run_test(values, source)
 
     def test_heydouga(self):
-        source = 'heydouga.com'
+        source = "heydouga.com"
         values = {
-            'heydouga 4240-009-3': ('heydouga-4240-009-3', '若菜亜衣', 1664150400),
-            'Heydouga 4030-PPV1768':
-            ('heydouga-4030-1768', '立花美涼', 1448150400),
-            'Heydouga 4030-PPV2232 AV9898':
-            ('heydouga-4030-2232', '極射', 1553904000),
-            'AV9898-1566': ('AV9898-1566', '前田由美', 1452643200),
-            'honnamatv-216 (5)': ('honnamatv-216-5', 'M顔娘', 1380585600)
+            "heydouga 4240-009-3": ("heydouga-4240-009-3", "若菜亜衣", 1664150400),
+            "Heydouga 4030-PPV1768": ("heydouga-4030-1768", "立花美涼", 1448150400),
+            "Heydouga 4030-PPV2232 AV9898": ("heydouga-4030-2232", "極射", 1553904000),
+            "AV9898-1566": ("AV9898-1566", "前田由美", 1452643200),
+            "honnamatv-216 (5)": ("honnamatv-216-5", "M顔娘", 1380585600),
         }
         self._run_test(values, source)
 
     def test_h4610(self):
-        source = 'h4610.com'
-        values = {'H4610 gol185': ('H4610-gol185', '美加子', 1497052800.0)}
+        source = "h4610.com"
+        values = {"H4610 gol185": ("H4610-gol185", "美加子", 1497052800.0)}
         self._run_test(values, source)
 
     def test_c0930(self):
-        source = 'c0930.com'
-        values = {'C0930-gol0136': ('C0930-gol0136', '羽田', 1456358400.0)}
+        source = "c0930.com"
+        values = {"C0930-gol0136": ("C0930-gol0136", "羽田", 1456358400.0)}
         self._run_test(values, source)
 
     def test_h0930(self):
-        source = 'h0930.com'
-        values = {'H0930 (ori1575)': ('H0930-ori1575', '33歳', 1593216000.0)}
+        source = "h0930.com"
+        values = {"H0930 (ori1575)": ("H0930-ori1575", "33歳", 1593216000.0)}
         self._run_test(values, source)
 
     def test_x1x(self):
-        source = 'x1x.com'
+        source = "x1x.com"
         values = {
-            'x1x-111815 一ノ瀬アメリ': ('x1x-111815', '50連発', 1396483200),
-            'x1x.com 111860': ('x1x-111860', '一ノ瀬アメリ', 1332374400),
+            "x1x-111815 一ノ瀬アメリ": ("x1x-111815", "50連発", 1396483200),
+            "x1x.com 111860": ("x1x-111860", "一ノ瀬アメリ", 1332374400),
         }
         self._run_test(values, source)
 
     def test_smmiracle(self):
-        source = 'sm-miracle.com'
-        values = {'sm miracle e0689': ('sm-miracle-e0689', '黒髪の地方令嬢２', None)}
+        source = "sm-miracle.com"
+        values = {"sm miracle e0689": ("sm-miracle-e0689", "黒髪の地方令嬢２", None)}
         self._run_test(values, source)
 
     def test_fc2(self):
-        source = 'fc2.com'
+        source = "fc2.com"
         values = {
-            'FC2-PPV-1021420_3': ('FC2-1021420-3', '32歳', 1548201600),
+            "FC2-PPV-1021420_3": ("FC2-1021420-3", "32歳", 1548201600),
         }
         self._run_test(values, source)
 
     def test_kin8(self):
-        source = 'kin8tengoku.com'
+        source = "kin8tengoku.com"
         values = {
-            'kin8-3039': ('kin8-3039', 'MASSAGE', 1548892800),
-            'Kin8tengoku 3329': ('kin8-3329', '肉感', 1607558400)
+            "kin8-3039": ("kin8-3039", "MASSAGE", 1548892800),
+            "Kin8tengoku 3329": ("kin8-3329", "肉感", 1607558400),
         }
         self._run_test(values, source)
 
     def test_girlsdelta(self):
-        source = 'girlsdelta.com'
-        values = {'GirlsDelta 1706': ('GirlsDelta-1706', '安原舞葉', None)}
+        source = "girlsdelta.com"
+        values = {"GirlsDelta 1706": ("GirlsDelta-1706", "安原舞葉", None)}
         self._run_test(values, source)
 
     def test_mgs(self):
-        source = 'mgstage.com'
+        source = "mgstage.com"
         values = {
-            'siro-1204': ('SIRO-1204', '体験撮影438', 1349136000),
-            'DANDY-241': ('DANDY-241', '風呂', 1308355200)
+            "siro-1204": ("SIRO-1204", "体験撮影438", 1349136000),
+            "DANDY-241": ("DANDY-241", "風呂", 1308355200),
         }
         self._run_test(values, source)
 
     def test_date(self):
-        source = 'date string'
+        source = "date string"
         values = {
-            'Ray Milf  28Jul2015 1080p': 1438041600,
-            'welivetogether.15.08.20.daisy.summers': 1440028800,
-            'welivetogether 23-jun 2014 test': 1403481600,
-            'welivetogether dec.23.2014 test': 1419292800,
-            'deeper.20.03.14.rae.lil.black': 1584144000,
-            'march 14, 2012': 1331683200,
+            "Ray Milf  28Jul2015 1080p": 1438041600,
+            "welivetogether.15.08.20.daisy.summers": 1440028800,
+            "welivetogether 23-jun 2014 test": 1403481600,
+            "welivetogether dec.23.2014 test": 1419292800,
+            "deeper.20.03.14.rae.lil.black": 1584144000,
+            "march 14, 2012": 1331683200,
             "20170102": 1483315200,
-            '20-03.14': None,
+            "20-03.14": None,
         }
         for k, v in values.items():
             result = scraper.scrape(k)
@@ -194,7 +202,6 @@ class Test_Scraper(unittest.TestCase):
 
 
 class Test_Idol(unittest.TestCase):
-
     def _run_test(self, wiki, values):
         for k, v in values.items():
             r = wiki.search(k)
@@ -209,62 +216,59 @@ class Test_Idol(unittest.TestCase):
     def test_wikipedia(self):
         wiki = idol.Wikipedia
         values = {
-            '鈴木さとみ': ('1988-09-09', {'鈴木さとみ', '浅田真美'}),
-            '上原結衣': ('1990-05-01', {'上原志織', '上原結衣'}),
-            '佐々木愛美': None,
+            "鈴木さとみ": ("1988-09-09", {"鈴木さとみ", "浅田真美"}),
+            "上原結衣": ("1989-10-10", {"上原志織", "上原結衣"}),
+            "佐々木愛美": None,
         }
         self._run_test(wiki, values)
 
     def test_minnanoav(self):
         wiki = idol.MinnanoAV
         values = {
-            '片瀬瑞穂': ('1993-04-12', {'成宮梓'}),
-            '上原志織': ('1990-05-01', {'上原結衣'}),
-            '蓮美': None,
+            "片瀬瑞穂": ("1993-04-12", {"成宮梓"}),
+            "上原志織": ("1990-05-01", {"上原結衣"}),
+            "蓮美": None,
         }
         self._run_test(wiki, values)
 
     def test_avrevolution(self):
         wiki = idol.AVRevolution
         values = {
-            '蓮美': (None, {'大高頼子', '鈴木ありさ'}),
-            '市川サラ': (None, {'市川サラ'}),
-            '伊藤ゆう': None,
+            "蓮美": (None, {"大高頼子", "鈴木ありさ"}),
+            "市川サラ": (None, {"市川サラ"}),
+            "伊藤ゆう": None,
         }
         self._run_test(wiki, values)
 
     def test_seesaawiki(self):
         wiki = idol.Seesaawiki
         values = {
-            '上原結衣': ('1989-10-10', {'上原志織', '上原結衣'}),
-            '成宮はるあ': ('1992-07-30', {'一ノ木ありさ', '乃木はるか'}),
-            '池田美和子': None,
+            "上原結衣": ("1989-10-10", {"上原志織", "上原結衣"}),
+            "成宮はるあ": ("1992-07-30", {"一ノ木ありさ", "乃木はるか"}),
+            "池田美和子": None,
         }
         self._run_test(wiki, values)
 
     def test_msin(self):
         wiki = idol.Msin
         values = {
-            '木内亜美菜': ('1991-11-30', {'木内亜美菜', '葉月美加子'}),
-            '今村ゆう': ('1996-03-15', {'沖野るり', '今村ゆう'}),
-            '前田ななみ': ('1993-04-12', {'片瀬瑞穂', '成宮梓'}),
+            "木内亜美菜": ("1991-11-30", {"木内亜美菜", "葉月美加子"}),
+            "今村ゆう": ("1996-03-15", {"沖野るり", "今村ゆう"}),
+            "前田ななみ": ("1993-04-12", {"片瀬瑞穂", "成宮梓"}),
         }
         self._run_test(wiki, values)
 
     def test_manko(self):
         wiki = idol.Manko
-        values = {
-            '南星愛': ('1996-01-31', {'南星愛'}),
-            '小司あん': (None, {'小司あん', '平子知歌'})
-        }
+        values = {"南星愛": ("1996-01-31", {"南星愛"}), "小司あん": (None, {"小司あん", "平子知歌"})}
         self._run_test(wiki, values)
 
     def test_etigoya(self):
         wiki = idol.Etigoya
         values = {
-            '市原さとみ': (None, {'鶴田沙織', '西村江梨', '由宇', '北野景子'}),
-            '上原志織': (None, {'上原志織', '上原結衣'}),
-            '佐々木愛美': (None, {'佐伯史華', '佐々木愛美'})
+            "市原さとみ": (None, {"鶴田沙織", "西村江梨", "由宇", "北野景子"}),
+            "上原志織": (None, {"上原志織", "上原結衣"}),
+            "佐々木愛美": (None, {"佐伯史華", "佐々木愛美"}),
         }
         for k, v in values.items():
             r = wiki.search(k)
@@ -286,9 +290,7 @@ class Test_Idol(unittest.TestCase):
 
 
 class Test_AVFile(unittest.TestCase):
-
     class DuckAVFile(video.AVFile):
-
         def __init__(self, **kwargs) -> None:
             for k, v in kwargs.items():
                 setattr(self, k, v)
@@ -322,7 +324,6 @@ class Test_AVFile(unittest.TestCase):
 
 
 class Test_Birth_List(unittest.TestCase):
-
     url = "http://www.minnano-av.com/actress_list.php?birthday=1989"
     tree = None
 
@@ -340,7 +341,6 @@ class Test_Birth_List(unittest.TestCase):
 
 
 class Test_Birth_Filter(unittest.TestCase):
-
     url = "http://www.minnano-av.com/actress.php?actress_id=9190"
     tree = None
 
@@ -363,5 +363,5 @@ class Test_Birth_Filter(unittest.TestCase):
             self.assertEqual(result, f"td[{v}]")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
